8a221c9c5cd2628cdc9633ab4244027ea9390888
Yatharth Kumar <78293379+yatharth811@users.noreply.github.com>
2022-01-29 16:27:04 +0530

    Updates

--- src/styles/TextLink.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-const TextLink = styled(Link)`
-  color: inherit;
-  text-decoration: none;
-`;
-
-export default TextLink;
